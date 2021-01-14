e5a0a5048872adbaa675f9006eb841ad00942ac0
Paulo Clavijo Esteban <paucls@gmail.com>
2021-01-14 11:54:02 +0000

    exercise 2 - step3 - endpoint implemented in catalog

--- catalog/src/main/kotlin/paucls/pactworkshop/catalog/api/ProductController.kt
+++ catalog/src/main/kotlin/paucls/pactworkshop/catalog/api/ProductController.kt
@@ -1,7 +1,10 @@
 package paucls.pactworkshop.catalog.api
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 import paucls.pactworkshop.catalog.app.Product
 import paucls.pactworkshop.catalog.app.ProductService
@@ -20,5 +23,11 @@ class ProductController(private val productService: ProductService) {
     @RequestMapping(value = ["/products/{id}"])
     fun getProduct(@PathVariable id: Int): Product {
         return productService.getProduct(id)
+    }
+
+    @RequestMapping(value = ["/products/{id}/favourite"], method = [RequestMethod.POST])
+    fun favouriteProduct(@PathVariable("id") id: Int): ResponseEntity<Unit> {
+        productService.favouriteProduct(id)
+        return ResponseEntity(HttpStatus.NO_CONTENT)
     }
 }
