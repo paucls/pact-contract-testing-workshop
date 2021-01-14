fa70ae0fde62a6d9f1b0e3d385a170a53de3c416
Paulo Clavijo Esteban <paucls@gmail.com>
2021-01-14 11:54:02 +0000

    exercise 2 - step3 - endpoint implemented in catalog

--- catalog/src/main/kotlin/paucls/pactworkshop/catalog/app/Product.kt
+++ catalog/src/main/kotlin/paucls/pactworkshop/catalog/app/Product.kt
@@ -4,7 +4,8 @@ data class Product(
         val id: Int,
         val name: String,
         val type: String,
-        var availability: ProductAvailability)
+        var availability: ProductAvailability,
+        var isFavouriteProduct: Boolean = false)
 
 enum class ProductAvailability {
     InStock,
--- catalog/src/main/kotlin/paucls/pactworkshop/catalog/persistence/InMemoryProductRepository.kt
+++ catalog/src/main/kotlin/paucls/pactworkshop/catalog/persistence/InMemoryProductRepository.kt
@@ -15,7 +15,7 @@ class InMemoryProductRepository : ProductRepository {
             Product(id = 0, name = "Permanent Marker", type = "STATIONERY", availability = ProductAvailability.InStock),
             Product(id = 1, name = "Colored Pencils Set", type = "STATIONERY", availability = ProductAvailability.OutOfStock),
             Product(id = 2, name = "Desk Phone", type = "ELECTRONICS", availability = ProductAvailability.InStock),
-            Product(id = 3, name = "USB Headphones", type = "ELECTRONICS", availability = ProductAvailability.OutOfStock))
+            Product(id = 3, name = "USB Headphones", type = "ELECTRONICS", availability = ProductAvailability.OutOfStock, isFavouriteProduct = true))
 
     override fun getAllProducts(): List<Product> {
         return products
